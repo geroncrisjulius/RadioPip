9a610920cceb0137ee4b3d5251ca3732a833adb2
Cris Julius Geron <geroncrisjulius@gmail.com>
2026-08-01 01:35:25 +0800

    Add excited

--- radpip/radpip.lua
+++ radpip/radpip.lua
@@ -1,3 +1,4 @@
+---@type TelemetryScript
 --------------------------------------------------
 -- RadioPip
 -- EdgeTx v2.12
@@ -33,11 +34,10 @@ local rxBatt = 0
 local rxDbm = 0
 local linkQuality = 0
 local throttle = 0
+
 ------------------------------------------------
 -- GET VALUES
 ------------------------------------------------
-
-
 
 local function getArmState()
   local armState = getValue(ARM_SWITCH)
@@ -47,28 +47,12 @@ local function getArmState()
   return armState > 0
 end
 
-local function getThrottle()
-  return getValue("thr")
-end
-
-local function getRxBatt()
-  return getValue("RxBt")
-end
-
-local function getRxDbm()
-  return getValue("1RSS")
-end
-
-local function getLinkQuality()
-  return getValue("RQly")
-end
-
 local function getTelemetryValues()
   armed = getArmState()
-  rxBatt = getRxBatt()
-  rxDbm = getRxDbm()
-  linkQuality = getLinkQuality()
-  throttle = getThrottle()
+  rxBatt = getValue("RxBt")
+  rxDbm = getValue("1RSS")
+  linkQuality = getValue("RQly")
+  throttle = getValue("thr")
 end
 ------------------------------------------------
 -- HELPER
@@ -107,45 +91,86 @@ local function isLinkQualityCrit()
 end
 
 local function percentThrottle()
-  return math.floor(((throttle + 1024) * 100)/2048) 
+  return math.floor(((throttle + 1024) * 100) / 2048)
 end
-
 
 ------------------------------------------------
 -- DRAW
 ------------------------------------------------
 local function drawSleepEye(x, y)
   drawSubPixel(x, y, 1, 1, FORCE)
-  drawSubPixel(x+1, y+1, 2, 1, FORCE)
-  drawSubPixel(x+3, y, 1, 1, FORCE)
+  drawSubPixel(x + 1, y + 1, 2, 1, FORCE)
+  drawSubPixel(x + 3, y, 1, 1, FORCE)
 end
-
-
 
 local function drawSleep(x, y)
   -- left eye
-  drawSleepEye(x, y+6)
+  drawSleepEye(x, y + 6)
 
   --right eye
-  drawSleepEye(x+8, y+6)
+  drawSleepEye(x + 8, y + 6)
 
   if (getTime() % 500) < 250 then
     --mouth
-    drawSubPixel(x+4, y+10, 4, 1, FORCE)
+    drawSubPixel(x + 4, y + 10, 4, 1, FORCE)
   else
     --mouth
-    drawSubPixel(x+5, y+10, 2, 2, FORCE)
+    drawSubPixel(x + 5, y + 10, 2, 2, FORCE)
   end
+end
+
+local function drawExcited(x, y)
+  local state = (getTime() % 300) < 150
+
+  -- left eye
+  local lx = x + 0
+  local ly = y + 0
+  if state then
+    drawSubPixel(lx, ly, 1, 1, FORCE)
+    drawSubPixel(lx + 1, ly + 1, 1, 1, FORCE)
+    drawSubPixel(lx + 2, ly + 2, 1, 1, FORCE)
+    drawSubPixel(lx + 1, ly + 3, 1, 1, FORCE)
+    drawSubPixel(lx, ly + 4, 1, 1, FORCE)
+  else
+    drawSubPixel(lx, ly + 3, 1, 1, FORCE)
+    drawSubPixel(lx + 1, ly + 2, 2, 1, FORCE)
+    drawSubPixel(lx + 3, ly + 3, 1, 1, FORCE)
+  end
+
+  -- right eye
+  local rx = x + 7
+  local ry = y + 0
+  if state then
+    drawSubPixel(rx + 2, ry, 1, 1, FORCE)
+    drawSubPixel(rx + 1, ry + 1, 1, 1, FORCE)
+    drawSubPixel(rx, ry + 2, 1, 1, FORCE)
+    drawSubPixel(rx + 1, ry + 3, 1, 1, FORCE)
+    drawSubPixel(rx + 2, ry + 4, 1, 1, FORCE)
+  else
+    rx = rx - 1
+    drawSubPixel(rx, ry + 3, 1, 1, FORCE)
+    drawSubPixel(rx + 1, ry + 2, 2, 1, FORCE)
+    drawSubPixel(rx + 3, ry + 3, 1, 1, FORCE)
+  end
+
+  --mouth
+  local mx = x + 0
+  local my = y + 7
+  drawSubPixel(mx, my, 10, 1, FORCE)
+  drawSubPixel(mx + 1, my + 1, 8, 1, FORCE)
+  -- drawSubPixel(mx + 8, my + 1, 1, 1, FORCE)
+  drawSubPixel(mx + 2, my + 2, 1, 1, FORCE)
+  drawSubPixel(mx + 7, my + 2, 1, 1, FORCE)
+  drawSubPixel(mx + 3, my + 3, 4, 1, FORCE)
 end
 
 local function drawBorders()
   lcd.drawFilledRectangle(0, 0, LCD_W, LCD_H, FORCE)
-  lcd.drawFilledRectangle(4, 4, 72, LCD_H-8, ERASE)
+  lcd.drawFilledRectangle(4, 4, 72, LCD_H - 8, ERASE)
   lcd.drawFilledRectangle(4, 4, 4, 4, FORCE)
   lcd.drawFilledRectangle(4, 56, 4, 4, FORCE)
   lcd.drawFilledRectangle(72, 4, 4, 4, FORCE)
   lcd.drawFilledRectangle(72, 56, 4, 4, FORCE)
-  
 end
 
 local function drawValues()
@@ -189,8 +214,18 @@ local function drawValues()
   lcd.drawLine(81, 55, 125, 55, DOTTED, FORCE)
   lcd.drawLine(81, 56, 125, 56, DOTTED, FORCE)
   lcd.drawFilledRectangle(81, 51, percentThrottle() * 44 / 100, 10, FORCE)
-  
 end
+
+local function drawPip()
+  if not armed then
+    drawSleep(4, 2)
+  elseif percentThrottle() > 50 then
+    drawExcited(5, 3)
+  else
+
+  end
+end
+
 
 ------------------------------------------------
 -- Main
@@ -199,13 +234,12 @@ end
 local function my_run(event)
   getTelemetryValues()
 
-  lcd.clear()   
+  lcd.clear()
   drawBorders()
 
-  drawSleep(4, 2)
+  drawPip()
 
   drawValues()
-
 end
 
 return { run = my_run }
